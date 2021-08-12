ebb37e5b8af9c366d50413d5e45db2f68145cfe1
Tao <2586638752@qq.com>
2021-08-12 21:30:13 +0800

    面向对象第二次练习

--- python_practice/python_class/oop.py
+++ python_practice/python_class/oop.py
@@ -7,16 +7,33 @@ class House:
     #静态属性->类变量（类之中，方法之外）
     door = "red"
     floor = "white"
+    #构造函数,在类实例化的时候就会执行
+    def __init__(self):
+        #第一种方法，在方法中调用类变量需要加self.
+        print(self.door)
+        # 实例变量，类的方法中，以“self.变量名”方法定义
+        self.kitchen = "cook"
     #动态方法
     def sleep(self):
-        print("在房子里可以睡觉")
+        # print("在房子里可以睡觉")
+
+        bed = "席梦思"
+        self.table = "桌子"
+        print(f"在房子里可以躺在{bed}上睡觉")
 
     def cook(self):
+
         print("在房子里可以做饭吃")
+        print(self.kitchen)
+        print(self.table)
 
 #把类实例化、
 #北欧
 north_house = House()
+north_house.sleep()
+#不调用sleep方法的时候会报错。调用sleepf方法里的self.变量的时候需要先调用sleep
+# AttributeError: 'House' object has no attribute 'table'
+north_house.cook()
 #中式
 China_house = House()
 #调用类的属性
